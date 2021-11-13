62693bb0df37cce84d850f8c50f8dd5970d8c6da
Ubuntu <ubuntu@ip-172-31-34-43.ec2.internal>
2021-11-13 00:02:10 +0000

    ortography issues

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-//axios.defaults.baseURL='http://ubuntu@ec2-52-7-202-195.compute-1.amazonaws.com:9997/api/'
-axios.defaults.baseURL='http://localhost:9997/api'
+axios.defaults.baseURL='http://ubuntu@ec2-52-7-202-195.compute-1.amazonaws.com:9997/api/'
+//axios.defaults.baseURL='http://localhost:9997/api'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 createApp(App).use(store).use(router).use(VueAxios,axios).mount('#app')
